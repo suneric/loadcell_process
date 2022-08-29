fda8c619f8b084c9fafeacc7bc2b7bd59f56b200
suneric <syf_more@163.com>
2022-08-29 14:46:36 -0400

    add loadcell 2

--- scripts/load_cell_interface.py
+++ scripts/load_cell_interface.py
@@ -13,9 +13,7 @@ from std_msgs.msg import Float32, Float32MultiArray, MultiArrayDimension
 REF = 5.08
 
 class mykalman():
-
     def __init__(self):
-
         self.sz = 1 # size of array
         self.Q = 1e-6 # process variance
         # allocate space for arrays
@@ -37,7 +35,6 @@ class mykalman():
          # time update
         self.xhatminus = self.A * self.xhat  #X(k|k-1) = AX(k-1|k-1) + BU(k) + W(k),A=1,BU(k) = 0
         self.Pminus = self.A * self.P+self.Q      #P(k|k-1) = AP(k-1|k-1)A' + Q(k) ,A=1
-
         # measurement update
         self.K = self.Pminus/( self.Pminus+self.R ) #Kg(k)=P(k|k-1)H'/[HP(k|k-1)H' + R],H=1
         self.xhat = self.xhatminus+self.K*(z-self.H * self.xhatminus) #X(k|k) = X(k|k-1) + Kg(k)[Z(k) - HX(k|k-1)], H=1
@@ -50,16 +47,29 @@ class LoadCellProcess:
         self.ADC = ADS1263.ADS1263()
         rospy.loginfo("Setting up the node")
         rospy.init_node("loadcell_ros_interface", anonymous=True)
-        self.force_pub = rospy.Publisher('/loadcell_forces', Float32MultiArray, queue_size=1)
-        self.fx=np.zeros((1, 100))
-        self.fy=np.zeros((1, 100))
-        self.fz=np.zeros((1, 100))
-        self.fx_filtered=np.zeros((1, 100))
-        self.fy_filtered=np.zeros((1, 100))
-        self.fz_filtered=np.zeros((1, 100))
-        self.kfx=mykalman()
-        self.kfy=mykalman()
-        self.kfz=mykalman()
+        self.force1_pub = rospy.Publisher('/loadcell1_forces', Float32MultiArray, queue_size=1)
+        self.force2_pub = rospy.Publisher('/loadcell2_forces', Float32MultiArray, queue_size=1)
+
+        self.kfx1=mykalman()
+        self.kfy1=mykalman()
+        self.kfz1=mykalman()
+        self.fx1=np.zeros((1, 100))
+        self.fy1=np.zeros((1, 100))
+        self.fz1=np.zeros((1, 100))
+        self.fx1_filtered=np.zeros((1, 100))
+        self.fy1_filtered=np.zeros((1, 100))
+        self.fz1_filtered=np.zeros((1, 100))
+
+        self.kfx2=mykalman()
+        self.kfy2=mykalman()
+        self.kfz2=mykalman()
+        self.fx2=np.zeros((1, 100))
+        self.fy2=np.zeros((1, 100))
+        self.fz2=np.zeros((1, 100))
+        self.fx2_filtered=np.zeros((1, 100))
+        self.fy2_filtered=np.zeros((1, 100))
+        self.fz2_filtered=np.zeros((1, 100))
+
         self.fx_zero= 0 #2.4543
         self.fy_zero= 0 #2.444
         self.fz_zero= 0 #2.454
@@ -69,47 +79,43 @@ class LoadCellProcess:
             return False
         else:
             self.ADC.ADS1263_SetMode(0)
-
             return True
+
+    def adc_raw(self,value):
+        if(value >> 31 == 1):
+            return -1*(REF*2 - value * REF / 0x80000000)
+        else:
+            return float((value * REF / 0x7fffffff))
 
     def acquire_data(self):
         ADC_Value = self.ADC.ADS1263_GetAll()    # get ADC1 value
-        # print(ADC_Value)
-        for i in range(0, 3):
-            if(ADC_Value[i]>>31 ==1):
-                if i==0:
-                    raw=-1*(REF*2 - ADC_Value[i] * REF / 0x80000000)
-                    self.fx=(raw-self.fx_zero)*200
-                    self.fx_filtered=self.kfx.update(self.fx)
-                if i==1:
-                    raw=-1*(REF*2 - ADC_Value[i] * REF / 0x80000000)
-                    self.fy=(raw-self.fy_zero)*200
-                    self.fy_filtered=self.kfy.update(self.fy)
-                if i==2:
-                    raw=-1*(REF*2 - ADC_Value[i] * REF / 0x80000000)
-                    self.fz=(raw-self.fz_zero)*200
-                    self.fz_filtered=self.kfz.update(self.fz)
-                #print("ADC1 IN%d = -%lf" %(i, (REF*2 - ADC_Value[i] * REF / 0x80000000)))
-            else:
-                #print("ADC1 IN%d = %lf" %(i, (ADC_Value[i] * REF / 0x7fffffff)))   # 32bit
-                if i==0:
-                    raw=float((ADC_Value[i] * REF / 0x7fffffff))
-                    self.fx=(raw-self.fx_zero)*200
-                    self.fx_filtered=self.kfx.update(self.fx)
-                if i==1:
-                    raw=float((ADC_Value[i] * REF / 0x7fffffff))
-                    self.fy=(raw-self.fy_zero)*200
-                    self.fy_filtered=self.kfy.update(self.fy)
-                if i==2:
-                    raw=float((ADC_Value[i] * REF / 0x7fffffff))
-                    self.fz=(raw-self.fz_zero)*200
-                    self.fz_filtered=self.kfz.update(self.fz)
+        for i in range(0, 6):
+            raw = self.adc_raw(ADC_Value[i])
+            if i==0:
+                self.fx1=(raw-self.fx_zero)*200
+                self.fx1_filtered=self.kfx1.update(self.fx1)
+            if i==1:
+                self.fy1=(raw-self.fy_zero)*200
+                self.fy1_filtered=self.kfy1.update(self.fy1)
+            if i==2:
+                self.fz1=(raw-self.fz_zero)*200
+                self.fz1_filtered=self.kfz1.update(self.fz1)
+            if i==3:
+                self.fx2=(raw-self.fx_zero)*200
+                self.fx2_filtered=self.kfx2.update(self.fx2)
+            if i==4:
+                self.fy2=(raw-self.fy_zero)*200
+                self.fy2_filtered=self.kfy2.update(self.fy2)
+            if i==5:
+                self.fz2=(raw-self.fz_zero)*200
+                self.fz2_filtered=self.kfz2.update(self.fz2)
 
     def publish_forces(self):
         self.acquire_data()
-        forces = Float32MultiArray(data=[self.fx_filtered,self.fy_filtered,self.fz_filtered])
-        self.force_pub.publish(forces)
-
+        forces1 = Float32MultiArray(data=[self.fx1_filtered,self.fy1_filtered,self.fz1_filtered])
+        self.force1_pub.publish(forces1)
+        forces2 = Float32MultiArray(data=[self.fx2_filtered,self.fy2_filtered,self.fz2_filtered])
+        self.force2_pub.publish(forces2)
 
     def run(self):
         if not self.initialize():
