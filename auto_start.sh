6ccd43c138c066ef9872582748ac9d50cbb47b9f
suneric <syf_more@163.com>
2023-03-15 15:33:45 -0400

    update auto start bash

--- auto_start.sh
+++ auto_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo -s -H << 'EOF'
+export ROS_HOSTNAME=ubuntu-desktop
 export ROS_MASTER_URI=http://192.168.1.19:11311
-export ROS_IP=192.168.1.15
 
 source /opt/ros/noetic/setup.bash
 source /home/ubuntu/catkin_ws/devel/setup.bash
